2b818a0f5ce7683e0cbb830dcf62de0c24366159
baek min gyu <msmk908@naver.com>
2023-12-29 11:34:33 +0900

    mini-blog

--- src/index.js
+++ src/index.js
@@ -3,12 +3,11 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import Button from './component/ui/Button';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Button />
+    <App />
   </React.StrictMode>
 );
 
